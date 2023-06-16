c9ac280078472720dd431d9d51dfb9d488e327df
ManuMira <63024342+ManuMira@users.noreply.github.com>
2023-06-16 19:28:27 -0300

    Create cadena.c
    Signed-off-by: ManuMira <63024342+ManuMira@users.noreply.github.com>

---
+++ cadena.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+        int n = 1;
+        char *str = {0};
+
+        str = (char *)malloc(n * sizeof(int));
+
+        printf("Inserte su cadena de arreglos: ");
+
+        scanf("\n%[^\n]", str);
+        printf("%s\n", str);
+
+free(str);
+
+return 0;
+}
+
+//PIDE al operador una cadena y la imprime en pantalla.
