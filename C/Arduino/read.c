12fae9000f2388d4b34eb85de893594dd3b3ee85
ManuMira <63024342+ManuMira@users.noreply.github.com>
2023-09-06 19:11:11 -0300

    Create read.c
    Signed-off-by: ManuMira <63024342+ManuMira@users.noreply.github.com>

---
+++ C/Arduino/read.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main (void){
+	int fd ; /*File descriptor*/
+	char buffer[20];
+	int i;
+	
+	fd = open("/dev/ttyS20", O_RDWR | O_NOCTTY);
+	if( fd == -1){
+		printf("ERROR: No se pudo abrir mi compa\n");
+		return -1;
+	}
+  
+	 i = read(fd, buffer, 6); //Timeout
+	 printf("Datos enviados pana (%d): %s\n",u, buffer);
+	 close(fd);
+	 
+	 return 0;
+}
