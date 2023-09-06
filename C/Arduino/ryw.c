24af035677ddb050af1f7e48ea7dab214200d469
ManuMira <63024342+ManuMira@users.noreply.github.com>
2023-09-06 19:01:37 -0300

    Create ryw.c
    Signed-off-by: ManuMira <63024342+ManuMira@users.noreply.github.com>

---
+++ C/Arduino/ryw.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+	int fd ; /*File descriptor*/
+	char buffer[20];
+	int i;
+	
+	fd = open("/dev/ttyS20", O_WRONLY);
+	if( fd == -1){
+		printf("ERROR: No se pudo abrir mi compa\n");
+		return -1;
+	}
+	 int n = write(fd, "Hola!", 6);
+	 printf("Datos enviados pana: %d\n",n);
+	 close(fd);
+	 
+	 return 0;
+}
