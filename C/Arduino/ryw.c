0e6ca6c9da244641507d22f79f431d4e4b2318ae
ManuMira <63024342+ManuMira@users.noreply.github.com>
2023-09-06 19:26:28 -0300

    Update ryw.c
    Signed-off-by: ManuMira <63024342+ManuMira@users.noreply.github.com>

--- C/Arduino/ryw.c
+++ C/Arduino/ryw.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main (void){
 	int fd ; /*File descriptor*/
@@ -11,8 +13,10 @@ int main (void){
 		printf("ERROR: No se pudo abrir mi compa\n");
 		return -1;
 	}
+	while(1){
 	 int n = write(fd, "Hola!", 6);
 	 printf("Datos enviados pana: %d\n",n);
+	 }
 	 close(fd);
 	 
 	 return 0;
