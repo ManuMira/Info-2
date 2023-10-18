d33f02138c0d071f2e4083929185258ce2597517
ManuMira <63024342+ManuMira@users.noreply.github.com>
2023-10-18 16:50:18 -0300

    Update motor.h
    Signed-off-by: ManuMira <63024342+ManuMira@users.noreply.github.com>

--- CPP/MOTOR/motor.h
+++ CPP/MOTOR/motor.h
@@ -1,18 +1,18 @@
-#ifndef MOT_H
+#ifndef MOT_H		//Definir una sola vez
 #define MOT_H
 
 class motor{
-	private:
-	int Velocidad_Actual;
-	int Velocidad_Objetivo;
+	private:		//Atributos
+	int Velocidad_Actual;	//int por ser entero 
+	int Velocidad_Objetivo;	//int por ser entero
 	
-	public:
-	motor(int Velocidad_Objetivo);
-	void set_Va(int Va);
-	int get_Va();
-	void set_Vo(int Vo);
-	int get_Vo();
-	void Incrementar();
-	void Decrementar();
+	public:			//Metodos
+	motor(int Velocidad_Objetivo);	//Constructor ordinario
+	void set_Va(int Va);		//void por ser un setter
+	int get_Va();			//int por sevolver un entero positivo
+	void set_Vo(int Vo);		//void por ser un setter
+	int get_Vo();			//int por sevolver un entero positivo
+	void Incrementar();		//void por ser un setter
+	void Decrementar();		//void por ser un setter
 }
 #endif
