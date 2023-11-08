1a9081bcc74a59da6b5c7c28c22707f221bebbe1
ManuMira <63024342+ManuMira@users.noreply.github.com>
2023-11-08 11:44:01 -0300

    Update Ej1.cpp
    Signed-off-by: ManuMira <63024342+ManuMira@users.noreply.github.com>

--- CPP/Herencia/Ej1.cpp
+++ CPP/Herencia/Ej1.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+#include <stdlib.h>
+
+class Persona{
+  private:
+    string nombre;
+    int edad;
+  public:
+    Persona(string, int);
+    mostrarPersona()const;
+    setNombre(string);
+    setEdad(int);
+}
+
+class Empleado : public Persona{
+  private:
+    int numEmpleado;
+    float sueldoEmpleado;
+  public:
+    Empleado(string, int, int, float);
+}
+
+class Alumno : public Persona{
+  protected:
+    int numAlumno;
+    int nota;
+  public:
+    Alumno(string, int, int, int);
+    mostrarAlumno();
+}
+
+class Universitario : public Alumno{
+  private:
+    int materiasAprob;
+  public:
+    Universitario(string, int, int, int, int);
+    mostrarUni();
+}
+
+Persona::Persona(string _nombre, int _edad){
+  nombre =_nombre;
+  edad = _edad;
+}
+
+Empleado::Empleado(string _nombre, int _edad, int _numEmpl, float _sueldoEmpl) : Persona(_nombre, _edad){
+  numEmpleado = _numEmpl;
+  sueldoEmpleado = _sueldoEmpl;
+}
+
+Alumno::Alumno(string _nombre, int _edad, int _numAlum, int _nota) : Persona(_nombre, _edad){
+  numAlumno = _numAlum;
+  nota = _nota;
+}
+Universitario::Universitario(string _nombre, int _edad, int _numAlum, _nota, int _materiasA) : Alumno(_numAlum, _nota){
+  materiasAprobadas = _materiasA;
+}
 
